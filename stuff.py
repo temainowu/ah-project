0d3b66e7c96e112df413d19c2fc128a0c3a34469
cosmobobak <cosmobobak@gmail.com>
2021-10-07 18:10:51 +0100

    formatting

--- stuff.py
+++ stuff.py
@@ -1,105 +1,119 @@
 import random
+from typing import Optional
 
-class Users:
+
+class User:
     def __init__(self, name: str, score: int):
         self.name = name
         self.score = score
-    #end def
+    # end def
 
-    def show(self):
+    def show(self) -> None:
         print(f'{self.name} has won {self.score} games')
-    #end def
-#end class
+    # end def
+# end class
+
 
 loclist = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-def sort(array):
+
+def sort(array: "list[User]") -> None:
     for i in range(1, len(array)):
-        temp = array[i] #swap
+        temp = array[i]  # swap
         pos = i
-        
+
         while pos > 0 and array[pos-1].score < temp.score:
-            array[pos] = array[pos-1] #swap
+            array[pos] = array[pos-1]  # swap
             pos -= 1
-        #end while
+        # end while
 
-        array[pos] = temp #swap
-    #end for
-    return array
-#end def
+        array[pos] = temp  # swap
+    # end for
+# end def
 
-def readScore():
+
+def parse_line(line: "list[str]") -> "User":
+    name, score = line
+    return User(name, int(score))
+
+
+def read_score() -> None:
     scores = []
-    file = open('Scores.txt','r')
+    file = open('Scores.txt', 'r')
     data = file.readlines()
     file.close()
 
     for line in data:
-        user = line.split(",")
-        scores.append(Users(user[0], int(user[1])))
-    #end for
+        user = parse_line(line.split(","))
+        scores.append(user)
+    # end for
 
-    scores = sort(scores)
+    sort(scores)
     for i in scores:
         i.show()
-    #end for
-#end def
+    # end for
+# end def
 
-def playMove(): #currently random (probably fine)
+
+def play_move() -> int:
+    # currently random (probably fine)
     move = random.randint(0, 8)
     while loclist[0][move] or loclist[1][move]:
         move = random.randint(0, 8)
-    #end while
+    # end while
     return move
-#end def
+# end def
 
-def search(array, target):
+
+def search(array: "list[User]", target: "str") -> "Optional[int]":
     found = False
     pos = 0
     for i in range(1, len(array)):
         if array[i].name == target:
             found = True
             pos = i
-        #end if
-    #end for
-    
+        # end if
+    # end for
+
     if found:
         return pos
     else:
         return None
-    #end if
-#end def
+    # end if
+# end def
 
-def addScore(name):
+
+def add_score(name: str) -> None:
     scores = []
-    file = open('Scores.txt','r')
+    file = open('Scores.txt', 'r')
     data = file.readlines()
     file.close()
 
     for line in data:
         user = line.split(",")
-        scores.append(Users(user[0], int(user[1])))
-    #end for
+        scores.append(User(user[0], int(user[1])))
+    # end for
 
     location = search(scores, name)
 
     if location is None:
-        scores.append(Users(name, 1))
+        scores.append(User(name, 1))
     else:
         scores[location].score += 1
-    #end if
+    # end if
 
     file = open('Scores.txt', 'w')
     file.truncate()
     for i in scores:
         file.write(f'{i.name},{i.score},\n')
-    #end for
+    # end for
     file.close()
-#end def
+# end def
 
-def gameOver(moveCount, name):
-    winCombos = [[1, 1, 1, 0, 0, 0, 0, 0, 0],
+
+def game_over(move_count: int, name: str) -> bool:
+    win_combos = [[1, 1, 1, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 1, 1, 1, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 1, 1, 1],
                  [1, 0, 0, 1, 0, 0, 1, 0, 0],
@@ -107,34 +121,36 @@ def gameOver(moveCount, name):
                  [0, 0, 1, 0, 0, 1, 0, 0, 1],
                  [1, 0, 0, 0, 1, 0, 0, 0, 1],
                  [0, 0, 1, 0, 1, 0, 1, 0, 0]]
-    for j in range(len(winCombos)):
-        if [x & y for x, y in zip(winCombos[j], loclist[0])] in winCombos:
-            print('You won in', (moveCount+1)//2, 'moves')
-            addScore(name)
+    for j in range(len(win_combos)):
+        if [x & y for x, y in zip(win_combos[j], loclist[0])] in win_combos:
+            print('You won in', (move_count+1)//2, 'moves')
+            add_score(name)
             return True
-        elif [x & y for x, y in zip(winCombos[j], loclist[1])] in winCombos:
-            print('AI won in', (moveCount)//2, 'moves')
+        elif [x & y for x, y in zip(win_combos[j], loclist[1])] in win_combos:
+            print('AI won in', (move_count)//2, 'moves')
             return True
-        #end if
-    #end for
-    
-    if moveCount > 8:
+        # end if
+    # end for
+
+    if move_count > 8:
         print('Draw')
         return True
     else:
         return False
-    #end if
-#end def
+    # end if
+# end def
 
-def show_board(board):
+
+def show_board(board: "list[str]") -> None:
     print(board[0], '|', board[1], '|', board[2])
     print('---------')
     print(board[3], '|', board[4], '|', board[5])
     print('---------')
     print(board[6], '|', board[7], '|', board[8])
-#end def
+# end def
 
-def play():
+
+def play() -> None:
     board = [' ', ' ', ' ',
              ' ', ' ', ' ',
              ' ', ' ', ' ']
@@ -144,31 +160,35 @@ def play():
 
     name = input('Name: ')
 
-    moveCount = 0
-    while not gameOver(moveCount, name):
-        move = moveCount&1 #%2
+    move_count = 0
+    while not game_over(move_count, name):
+        move = move_count & 1  # (same as doing move_count % 2)
         if move:
-            loc = playMove()
+            loc = play_move()
         else:
             loc = int(input('Enter move: '))-1
             while loclist[0][loc] or loclist[1][loc]:
                 print('invalid move')
                 loc = int(input('Enter move: '))-1
-            #end while
-        #end if
+            # end while
+        # end if
         board[loc] = symbol[move]
         loclist[move][loc] = 1
         show_board(board)
-        moveCount += 1
-    #end while
-#end def
+        move_count += 1
+    # end while
+# end def
+
 
 def main():
     if input('Play Game? (Y/N): ') == 'Y':
         play()
     elif input('View scores? (Y/N): ') == 'Y':
-        readScore()
-    #end if
-#end main
+        read_score()
+    # end if
+# end main
 
-main()
+
+if __name__ == '__main__':
+    main()
+# end if
